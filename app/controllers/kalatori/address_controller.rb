0227293697239985702418161795022f80054790
Kirill Pimenov <kirill@pimenov.cc>
2024-03-02 20:47:20 +0100

    Passes order_id to the Kalatori daemon

--- app/controllers/kalatori/address_controller.rb
+++ app/controllers/kalatori/address_controller.rb
@@ -1,4 +1,16 @@
+require 'httparty'
+
 class Kalatori::AddressController < ApplicationController
+  include Spree::Core::ControllerHelpers::Store 
+  include Spree::Core::ControllerHelpers::Order
+  
+  SHOP_ID = 1
+
   def generate
+    # Getting current shopping cart price from session
+    @order = current_order(create_order_if_necessary: true)
+    
+    response = HTTParty.get("http://localhost:4010/order/#{SHOP_ID}/price/#{@order.id}")
+    render json: response
   end
 end
