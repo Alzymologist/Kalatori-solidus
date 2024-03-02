d239c9be538ff60da50768c60a8b36c27afc68a9
Kirill Pimenov <kirill@pimenov.cc>
2024-03-02 16:15:00 +0100

    Initial commit: generated project with `solidus extension`

---
+++ lib/solidus_kalatori.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'solidus_kalatori/configuration'
+require 'solidus_kalatori/version'
+require 'solidus_kalatori/engine'
---
+++ lib/solidus_kalatori/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SolidusKalatori
+  VERSION = '0.0.1'
+end
